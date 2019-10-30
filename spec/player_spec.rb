a4ad02a34a694d6eff4204c7838d5a45c639ae7c
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-30 11:11:42 +0000

    :white_check_mark: Increase test coverage for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,4 +5,12 @@ describe Player do
   subject(:player) { Player.new(name: name) }
 
   it { is_expected.to respond_to(:name) }
+
+  it "instantiates the player class" do
+    expect(player).to be_instance_of(Player)
+  end
+
+  it "returns the name of the player" do
+    expect(player.name).to eq "Negan"
+  end
 end
